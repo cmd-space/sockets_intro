e2ab49aa341e12e7bb8d2606418951cb741e0aa8
mec64 <masonecrane@gmail.com>
2015-05-09 14:23:35 -0600

    created package.json, added index.ejs and installed socket.io to mess around while using sockets

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static(path.join(__dirname, './static')));
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+var server = app.listen(8000, function(){
+    console.log('listening on port 8000');
+});
+
+var io = require('socket.io').listen(server);
+// whenever a connection event happens run the following code
+io.sockets.on('connection', function(socket){
+    console.log('we are using sockets!');
+    console.log(socket.id);
+});
